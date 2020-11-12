c0a6804b597208eda96ff0bf95706212aa760cb4
farah <farahaiman548@gmail.com>
2020-11-12 11:56:58 +0500

    solve all error :)

--- src/Chaild.js
+++ src/Chaild.js
@@ -5,6 +5,7 @@ function Chaild(props) {
         <div>
             <h2> Great but sakht mehnat ki zarort h {props.sirReviwe}</h2>
             <h2> YOU Will do it {props.sirBestIdea}</h2>
+            <h2> programing asan nhi h error ka dariya doob k jana h  {props.sirBestIdea}</h2>
         </div>
     )
 }
